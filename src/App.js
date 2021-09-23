bd42b61f58d947772a23ad155d61110a0b0efacc
Filipe <queiroz.filipe94@gmail.com>
2021-09-23 18:36:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <div className='display'>125</div>
+        <button className='clear'>Clear</button>
+        <div className='whiteSpace'></div>
+        <button className='division'>&#247;</button>
+        <button className='multiply'>&#215;</button>
+        <button className='add'>+</button>
+        <button className='subtract'>-</button>
+        <button className='equal'>=</button>
+        <button className='one'>1</button>
+        <button className='two'>2</button>
+        <button className='three'>3</button>
+        <button className='four'>4</button>
+        <button className='five'>5</button>
+        <button className='six'>6</button>
+        <button className='seven'>7</button>
+        <button className='eight'>8</button>
+        <button className='nine'>9</button>
+        <button className='zero'>0</button>
+        <button className='decimal'>.</button>
+      </div>
+    );
+  }
 }
-
-export default App;
